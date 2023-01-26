2d17b5031d624e80f4d1b93b93cbc1c7c3034e30
James Struga <jstruga@rocketsoftware.com>
2023-01-26 14:58:12 -0500

    add logging
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    more debug
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    add github file
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    typo
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    add new logic
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    add function in utils
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    more test
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    more test
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    finish logic
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    fix logic
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    test package.json logic
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    test package.json logic
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    add test
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    add missing var
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    add logging
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    add logging
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    add logging
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    add logging
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    add logging
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    add logging
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    add logging
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    add logging
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    add logging
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    add logging
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>
    
    add logging
    
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>

--- plugins/bump-version/index.js
+++ plugins/bump-version/index.js
@@ -1,24 +1,83 @@
 const core = require('@actions/core')
 const utils = require('./lib/utils.js')
+const github = require('./lib/github.js')
 const Debug = require('debug')
 const actionsGithub = require('@actions/github')
 
 
 
-
-var branch = 'v2.x/staging'
+var version = core.getInput('version')
+var branch = core.getInput('branch')
 var repo = actionsGithub.context.repo.owner + '/' + actionsGithub.context.repo.repo
 
+if (branch == ''){
+	branch = 'v2.x/staging'
+}
+if (version == '') {
+    version = 'MINOR'
+}
 
 
 // get temp folder for cloning
 var tempFolder = `${process.env.RUNNER_TEMP}/.tmp-npm-registry-${utils.dateTimeNow()}`
+console.log(`${tempFolder}`)
 
 console.log(`Cloning ${branch} into ${tempFolder} ...`)
 // clone to temp folder
 github.clone(repo,tempFolder,branch)
 
-console.log(`repo: ${repo}, tmp: ${tempFolder}, branch: ${branch}`)
-
 // run npm version
 console.log(`Making a "${version}" version bump ...`)
+
+
+//test 
+var newVersion
+var res
+var workdir = tempFolder;
+var manifest
+
+
+// bump package.json 
+packageNames = utils.findAllPackages(`${workdir}`)
+packageDir = packageNames.split(' ')
+
+for (let i = 0; i < packageDir.length; i++){
+	utils.bumpPackageVersion(`${workdir}/${packageDir[i]}`,version)
+	console.log(utils.sh(`cat ${workdir}/${packageDir[i]}`));
+}
+
+
+if (utils.fileExists(workdir + '/manifest.yaml')) {
+	manifest = 'manifest.yaml'
+} else if (utils.fileExists(workdir + '/manifest.yml')) {
+	manifest = 'manifest.yml'
+} else if (utils.fileExists(workdir + '/manifest.json')) {
+	throw new Error('Bump version on manifest.json is not supported yet.')
+} else {
+	throw new Error('No manifest found.')
+}
+
+
+newVersion = utils.bumpManifestVersion(`${workdir}/${manifest}`, version)
+console.log('New version:', newVersion)
+github._cmd(tempFolder, 'status');
+github._cmd(tempFolder, 'diff');
+github.add(workdir, 'manifest.yaml')
+for (let i = 0; i < packageDir.length; i++){
+	github.add(workdir, ` -f ${packageDir[i]}`)
+}
+res = github.commit(tempFolder, newVersion)
+
+
+if (res.includes('Git working directory not clean.')) {
+	throw new Error('Working directory is not clean')
+} else if (!newVersion.match(/^v[0-9]+\.[0-9]+\.[0-9]+$/)) {
+	throw new Error(`Bump version failed: ${newVersion}`)
+}
+
+
+console.log(`Pushing ${branch} to remote ...`)
+github.push(branch, tempFolder, actionsGithub.context.actor, process.env.GITHUB_TOKEN, repo)
+if (!github.isSync(branch, tempFolder)) {
+	throw new Error('Branch is not synced with remote after npm version.')
+}
--- plugins/bump-version/lib/utils.js
+++ plugins/bump-version/lib/utils.js
@@ -36,25 +36,32 @@ class utils {
 
         return version;
     }
+
+	static fileExists(path, quiet) {
+        try {
+            fs.accessSync(path, fs.constants.F_OK)
+            if (!quiet) {console.log(`${path} exists :D `)}
+            return true
+        } catch {
+            if (!quiet) {console.warn(`${path} does not exist :(`)}
+            return false
+        }
+    }
 	
-	static bumpManifestVersion() {
-		
-        const version = 'MINOR'
-        
+	static bumpManifestVersion(manifest, version) {
+        if (version == '') {
+            version = 'MINOR';
+        }
 
-
-        const oldVersionLine = this.sh(`cat manifest.yaml | grep 'version:'`);
+        const oldVersionLine = this.sh(`cat ${manifest} | grep 'version:'`);
         if (!oldVersionLine) {
             console.log(`Version is not defined in ${manifest}`);
             return;
         }
-		
         const oldVersion = oldVersionLine.split(':')[1].trim();
         let oldVersionParsed = this.parseSemanticVersion(oldVersion);
-		
-		console.log(`parseS Version is ${oldVersionParsed}`);
-        
-		switch (version.toUpperCase()) {
+
+        switch (version.toUpperCase()) {
             case 'PATCH':
                 oldVersionParsed['patch'] = parseInt(oldVersionParsed['patch'], 10) + 1;
                 break;
@@ -70,11 +77,32 @@ class utils {
         }
         const newVersion = this.combineSemanticVersion(oldVersionParsed);
 
-        const manifestContent = fs.readFileSync('manifest.yaml').toString();
-        fs.writeFileSync('manifest.yaml', manifestContent.replace(/^version:.*$/m, `version: ${newVersion}`));
-		
-		return `v${oldVersion}`;
+        const manifestContent = fs.readFileSync(manifest).toString();
+        fs.writeFileSync(manifest, manifestContent.replace(/^version:.*$/m, `version: ${newVersion}`));
+
+        return `v${newVersion}`;
     }
+	
+	static findAllPackages(directory){
+		const packageNames = utils.sh(`cd ${directory} && echo $(find . -name package.json | { grep -v node_modules || true; })`);
+		return packageNames
+	}
+	
+	static bumpPackageVersion(packageFile, version){
+		if (version == '') {
+            version = 'MINOR';
+        }
+		
+		if (version == 'PATCH'){
+			this.sh(`sh -c "cd \`dirname ${packageFile}\` && npm version patch --force"`)
+		} else if (version == 'MINOR'){
+			this.sh(`sh -c "cd \`dirname ${packageFile}\` && npm version minor --force"`)
+		} else if (version == 'MAJOR'){
+			this.sh(`sh -c "cd \`dirname ${packageFile}\` && npm version major --force"`)
+		} else {
+			console.log(`Error: version seems to be missing`)
+		}
+	}
 	
 }
 
